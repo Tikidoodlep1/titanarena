2350fc2e53d0771e99c908a3f78f0b3cd653b1ca
Unknown <alex.x.eye@gmail.com>
2019-12-08 15:03:22 -0600

    Iron falon fixed

--- scripts/vscripts/iron_talon.lua
+++ scripts/vscripts/iron_talon.lua
@@ -9,18 +9,3 @@ local ability = keys.ability
 		ability:ApplyDataDrivenModifier(caster, caster, "iron_talon_stats_ranged", {duration=-1})
 	end
 end
-
-function Quell(keys)
-local caster = keys.caster
-local target = keys.target
-local ability = keys.ability
-local cd = ability:GetSpecialValueFor("alternative_cooldown")
-
-	if target:GetEntityIndexForTreeId() then
-			ability:StartCooldown(cd)
-	else
-	end
-	if target:IsCreature() == true then
-		ability:ApplyDataDrivenModifier(caster, target, "iron_talon_damage", {})
-	end
-end
