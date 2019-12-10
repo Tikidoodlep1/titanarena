6eb66fc2ff01f70b7e7429514681f2853fb19cb5
Unknown <alex.x.eye@gmail.com>
2019-12-09 22:56:52 -0600

    fixed drops

--- scripts/vscripts/events.lua
+++ scripts/vscripts/events.lua
@@ -377,21 +377,6 @@ function barebones:OnEntityKilled(keys)
 		RollDrops(killed_unit)
 	end
 	
-	function RollDrops(unit)
-		local dropinfo = GameRules.DropTable[unit:GetUnitName()]
-		if dropinfo then
-			for item_name, chance in pairs(dropinfo) do
-				if RollPercentage(chance) then
-					local item = CreateItem(item_name, nil, nil)
-					local pos = unit:GetAbsOrigin()
-					local drop = CreateItemOnPositionSync(pos, item)
-					local pos_launch = pos+RandomVector(RandomFloat(150,200))
-					item:LaunchLoot(false, 200, 0.75, pos_launch)
-				end
-			end
-		end
-	end
-	
 	if keys.entindex_attacker ~= nil then
 		killer_unit = EntIndexToHScript(keys.entindex_attacker)
 	end
@@ -547,6 +532,36 @@ function barebones:OnEntityKilled(keys)
 	end
 end
 
+function RollDrops(unit)
+		local dropinfo = GameRules.DropTable[unit:GetUnitName()]
+		if dropinfo then
+			for k,ItemTable in pairs(dropinfo) do
+				local item_name
+				if ItemTable.ItemSets then
+					local count = 0
+					for i, v in pairs(ItemTable.ItemSets) do
+						count = count + 1
+					end
+					local rand = RandomInt(1, count)
+					item_name = ItemTable.ItemSets[tostring(rand)]
+				else
+					item_name = ItemTable.Item
+					local chance = ItemTable.Chance or 100
+					local max_drops = ItemTable.Multiple or 1
+					for i=1, max_drops do
+						if RollPercentage(chance) then
+							local item = CreateItem(item_name, nil, nil)
+							local pos = unit:GetAbsOrigin()
+							local drop = CreateItemOnPositionSync(pos, item)
+							local pos_launch = pos+RandomVector(RandomFloat(150,200))
+							item:LaunchLoot(false, 200, 0.75, pos_launch)
+						end
+					end
+				end
+			end
+		end
+	end
+	
 -- This function is called once when the player fully connects and becomes "Ready" during Loading
 function barebones:OnConnectFull(keys)
 	DebugPrint("[BAREBONES] A Player fully connected.")
