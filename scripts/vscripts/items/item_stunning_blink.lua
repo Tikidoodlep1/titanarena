6303f974693aae9a7a6e4653049d1f5689bbd5a3
Unknown <superdotaplaya@gmail.com>
2019-12-11 15:03:39 -0500

    Added stunning blink dagger item (abyssal blink?) as well as particle and images for icons

---
+++ scripts/vscripts/items/item_stunning_blink.lua
@@ -0,0 +1,50 @@
+function item_blink_datadriven_on_spell_start(keys)
+	ProjectileManager:ProjectileDodge(keys.caster)  --Disjoints disjointable incoming projectiles.
+	
+	ParticleManager:CreateParticle("particles/items_fx/blink_dagger_start.vpcf", PATTACH_ABSORIGIN, keys.caster)
+	keys.caster:EmitSound("DOTA_Item.BlinkDagger.Activate")
+	
+	local origin_point = keys.caster:GetAbsOrigin()
+	local target_point = keys.target_points[1]
+	local difference_vector = target_point - origin_point
+	local caster = keys.caster
+	local ability = keys.ability
+	local stun_duration = ability:GetSpecialValueFor("stun_duration")
+	
+	if difference_vector:Length2D() > keys.MaxBlinkRange then  --Clamp the target point to the BlinkRangeClamp range in the same direction.
+		target_point = origin_point + (target_point - origin_point):Normalized() * keys.BlinkRangeClamp
+	end
+	
+	keys.caster:SetAbsOrigin(target_point)
+	FindClearSpaceForUnit(keys.caster, target_point, false)
+	
+	ParticleManager:CreateParticle("particles/items_fx/blink_dagger_end.vpcf", PATTACH_ABSORIGIN, keys.caster)
+	local units = FindUnitsInRadius(caster:GetTeamNumber(), target_point, nil, 300 , DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+	for number,entity in pairs(units) do
+			if entity:IsHero() then
+				print(entity)
+							ability:ApplyDataDrivenModifier(caster, entity, "modifier_stunning_blink_stun", {duration = stun_duration})
+				return
+			end
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when a unit with Blink Dagger in their inventory takes damage.  Puts the Blink Dagger on a brief cooldown
+	if the damage is nonzero (after reductions) and originated from any player or Roshan.
+	Additional parameters: keys.BlinkDamageCooldown and keys.Damage
+	Known Bugs: keys.Damage contains the damage before reductions, whereas we want to compare the damage to 0 after reductions.
+================================================================================================================= ]]
+function modifier_item_blink_datadriven_damage_cooldown_on_take_damage(keys)
+	local attacker_name = keys.attacker:GetName()
+
+	if keys.Damage > 0 and (attacker_name == "npc_dota_roshan" or keys.attacker:IsControllableByAnyPlayer()) then  --If the damage was dealt by neutrals or lane creeps, essentially.
+		if keys.ability:GetCooldownTimeRemaining() < keys.BlinkDamageCooldown then
+			keys.ability:StartCooldown(keys.BlinkDamageCooldown)
+
+		end
+	end
+end
