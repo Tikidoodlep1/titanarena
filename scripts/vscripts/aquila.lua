a27b62f82f5a2376ff642b489ab82cb392183818
Unknown <alex.x.eye@gmail.com>
2019-12-11 19:29:18 -0600

    made lua item files in the item forlder

--- scripts/vscripts/aquila.lua
+++
@@ -1,9 +0,0 @@
-function ApplyAura(keys)
-local caster = keys.caster
-local ability = keys.abilty
-	if caster:HasModifier("apply_aquila_modifier") == false then
-		ability:ApplyDataDrivenModifier(caster, caster, "apply_aquila_modifier", {duration = -1})
-	else
-		ability:RemoveModifierByNameAndCaster("apply_aquila_modifier", caster)
-	end
-end
--- scripts/vscripts/edeso.lua
+++
@@ -1,8 +0,0 @@
-function CheckRanged(keys)
-local caster = keys.caster
-local ability = keys.ability
-
-	if caster:IsRangedAttacker() == true then
-		ability:ApplyDataDrivenModifier(caster, caster, "ranged_modifier", {duration = -1})
-	end
-end
