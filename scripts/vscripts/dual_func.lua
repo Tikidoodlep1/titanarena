04994e5fbd4a9022e26b45fa986bc877df84977f
Unknown <alex.x.eye@gmail.com>
2019-12-22 15:04:46 -0600

    same as last

--- scripts/vscripts/dual_func.lua
+++ scripts/vscripts/dual_func.lua
@@ -9,7 +9,6 @@ function TeleportOut(trigger)
 local playerloc = trigger.activator:GetAbsOrigin()
 local closest = Entities:FindByNameNearest("dual_keepout", playerloc, 5000):GetAbsOrigin()
 	trigger.activator:SetMana(_G.DualTeleportTargetManaOut)
-	print("setting mana to ".._G.DualTeleportTargetMana)
 	FindClearSpaceForUnit(trigger.activator, closest, false)
 	SendToConsole("dota_camera_center")
 end
@@ -18,7 +17,6 @@ function InDuals(trigger)
 local point = Entities:FindByName(nil, trigger.caller:GetName()):GetAbsOrigin()
 
 _G.DualTeleportTargetMana = trigger.activator:GetMana()
-print("current mana is ".._G.DualTeleportTargetMana)
 trigger.activator:SetMana(0)
 end
 
@@ -45,7 +43,7 @@ for i, hero in pairs(_G.DualArenavs2) do
 	end
 end
 trigger.activator:SetMana(_G.DualTeleportTargetMana)
-print("setting mana to ".._G.DualTeleportTargetMana)
 FindClearSpaceForUnit(trigger.activator, closest, false)
 SendToConsole("dota_camera_center")
 end
+
