83868e42f5f16f2e51cc79601589cca7f8fb3d05
Unknown <superdotaplaya@gmail.com>
2019-12-24 17:06:59 -0500

    triggers work to teleport wanderer back to spawn point, and script is properly working as well

--- scripts/vscripts/wanderer_warp_back.lua
+++ scripts/vscripts/wanderer_warp_back.lua
@@ -1,4 +1,12 @@
 function warp_check(trigger)
+local unit = trigger.activator
 
+if unit:GetUnitName() == "npc_boss_wanderer" then 
+local point = Entities:FindByName(nil, "wanderer_spawn"):GetAbsOrigin()
+FindClearSpaceForUnit(unit, point, false)
+print("Wanderer has crossed the trigger, and has been teleported back to its spawn point!")
+
+else
 print("wanderer not found")
+end
 end
