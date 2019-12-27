d472a13abe080ab7e47f825873a9b9f7f2642a61
Unknown <superdotaplaya@gmail.com>
2019-12-27 00:51:57 -0500

    fixed titans not returning to spawnpoint if target gets far away from them

--- scripts/vscripts/titan_ai.lua
+++ scripts/vscripts/titan_ai.lua
@@ -2,7 +2,7 @@ require('gamemode')
 function Spawn( entityKeyValues )
 
 
-	thisEntity:SetContextThink( "dual_actions", dual_actions , 2)
+	thisEntity:SetContextThink( "dual_actions", dual_actions , 1)
 	print("Starting AI for "..thisEntity:GetUnitName().." "..thisEntity:GetEntityIndex())
 
 end
@@ -14,35 +14,36 @@ function dual_actions()
 	if _G.IsDual == true and thisEntity:GetTeamNumber() == 2 then
 			thisEntity:MoveToTargetToAttack(Entities:FindByName(nil, "npc_dire_titan"))
 			print("Radiant titan is targeting dire titan")
-			return 2
+			return 1
 		end
 	if _G.IsDual == true and thisEntity:GetTeamNumber() == 3 then
 			thisEntity:MoveToTargetToAttack(Entities:FindByName(nil, "npc_radiant_titan"))
 			print("Dire titan is targeting radiant titan")
-			return 2
+			return 1
 	end
-	if _G.IsDual == false and thisEntity:GetAggroTarget() == nil then
-		if thisEntity:GetAggroTarget() ~= nil then
+	if _G.IsDual == false and thisEntity:GetAggroTarget() ~= nil then
 		local distance_from_target = (thisEntity:GetOrigin() - thisEntity:GetAggroTarget():GetOrigin()):Length2D()
-			
+			print(distance_from_target)
 			if distance_from_target >= 800 then
 					thisEntity:SetAggroTarget(nil)
-					return 2
+					return 1
 			end
+			return 1
 		end
-			if thisEntity:GetTeamNumber() == 2 then
+	if _G.IsDual == false and thisEntity:GetAggroTarget() == nil then
+					if thisEntity:GetTeamNumber() == 2 then
 					thisEntity:MoveToPosition(Entities:FindByName(nil, "rad_titan"):GetOrigin())
 					print("Radiant titan is returning to their spawn location")
-					return 2
+					return 1
 			end
 
 			if thisEntity:GetTeamNumber() == 3 then
 				thisEntity:MoveToPosition(Entities:FindByName(nil, "dire_titan"):GetOrigin())
 				print("Dire titan is returning to their spawn location")
-				return 2
+				return 1
 			end
 
 	end
-	return 2
+	return 1
 end
 
