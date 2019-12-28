9c6a258f1f77cb62fbdfd5bc47885c5ac1f17297
Unknown <superdotaplaya@gmail.com>
2019-12-28 02:36:33 -0500

    ai now custs ult when below 99% hp (for testing purposes) ability is broken and needs fixed for it to do anything

--- scripts/vscripts/wanderer_ai.lua
+++ scripts/vscripts/wanderer_ai.lua
@@ -1,6 +1,6 @@
 require('gamemode')
 function Spawn( entityKeyValues )
-
+	ABILITY_blood_pact = thisEntity:FindAbilityByName("wanderer_blood_pact")
 
 	thisEntity:SetContextThink( "roam", roam , 2)
 	print("Starting AI for "..thisEntity:GetUnitName().." "..thisEntity:GetEntityIndex())
@@ -21,17 +21,20 @@ thisEntity:MoveToPositionAggressive(Vector(go_to_x,go_to_y,281))
 return 2
 	end 
 if thisEntity:GetAggroTarget() ~= nil then
+	if (thisEntity:GetHealth() / thisEntity:GetMaxHealth()) <= .99 then
+print("casting ult")
+	thisEntity:Stop()
+	thisEntity:CastAbilityOnTarget(thisEntity:GetAggroTarget(), ABILITY_blood_pact , -1)
+
+
+
+return 21
+	end
 local distance_from_target = (thisEntity:GetOrigin() - thisEntity:GetAggroTarget():GetOrigin()):Length2D()
 if distance_from_target >= 750 then
 	thisEntity:SetAggoTarget(nil)
-return 2
+return .1
 end
-if (thisEntity:GetHealth() / thisEntity:GetMaxHealth()) <= .99 then
-	local ABILITY_blood_pact = thisEntity:FindAbilityByName("wanderer_blood_pact")
-	thisEntity:Stop()
-thisEntity:CastAbilityOnTarget(thisEntity:GetAggroTarget(), ABILITY_blood_pact , -1)
 
-return 5
-	end
 end
 end
