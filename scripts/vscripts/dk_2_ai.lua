46f92f2ba4df3d383193e05840647c414a7ab4d9
Unknown <alex.x.eye@gmail.com>
2020-01-06 00:36:02 -0500

    dk_demon_form works, I don't think it willl change him into his black dragon form though, need to find out how to do that

--- scripts/vscripts/dk_2_ai.lua
+++ scripts/vscripts/dk_2_ai.lua
@@ -1,7 +1,7 @@
 require('gamemode')
 function Spawn( entityKeyValues )
     ABILITY_toxin = thisEntity:FindAbilityByName("dk_toxin")
-    
+    ABILITY_demon_form = thisEntity:FindAbilityByName("dk_demon_form")
 
     thisEntity:SetContextThink( "cast", cast , 1)
     print("Starting AI for "..thisEntity:GetUnitName().." "..thisEntity:GetEntityIndex())
@@ -14,6 +14,11 @@ function cast()
     thisEntity:CastAbilityOnPosition(thisEntity:GetAggroTarget():GetAbsOrigin(), ABILITY_toxin , -1)
 	ParticleManager:CreateParticle("particles/dk_toxin.vpcf", PATTACH_ABSORIGIN, thisEntity:GetAggroTarget())
 return 21
+	if (thisEntity:GetHealth() / thisEntity:GetMaxHealth()) <= .50 then
+    thisEntity:Stop()
+    thisEntity:CastAbilityNoTarget(ABILITY_demon_form , -1)
+	ParticleManager:CreateParticle("particles/units/heroes/hero_dragon_knight/dragon_knight_transform_black.vpcf", PATTACH_ABSORIGIN, thisEntity:GetAbsOrigin())
+return 51
     end
     if thisEntity:GetAggroTarget() ~= nil then
 local distance_from_target = (thisEntity:GetOrigin() - thisEntity:GetAggroTarget():GetOrigin()):Length2D()
