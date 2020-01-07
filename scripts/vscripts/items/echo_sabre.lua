e6dfa20e3eb4291839d164a6135bb5352f43d760
Unknown <superdotaplaya@gmail.com>
2020-01-07 01:15:30 -0500

    Added tooltips for echo sabre 2 and debuff, added recipe for echo sabre 2, and updated stats for echo sabre 2

--- scripts/vscripts/items/echo_sabre.lua
+++ scripts/vscripts/items/echo_sabre.lua
@@ -6,18 +6,18 @@ local ability = keys.ability
 local caster = keys.caster
 local enemy_target = keys.target
 local abil_cooldown = ability:GetSpecialValueFor("AbilityCooldown")
-print("checking echo sabre 2")
+
 if ability:GetCooldownTimeRemaining() == 0 then
-	ability:ApplyDataDrivenModifier(caster, enemy_target, "modifier_echo_sabre_debuff", {duration = .8})
+	ability:ApplyDataDrivenModifier(caster, enemy_target, "modifier_echo_sabre_2_debuff", {duration = .8})
 	ability:StartCooldown(5)
 	local attacks_sent = 0
-print("echo sabre 2 is off cooldown")
+
 
 	Timers:CreateTimer(.3, function()
 	if attacks_sent <= 1 then
 		 StartAnimation(caster, {duration=1.5, activity=ACT_DOTA_ATTACK, rate=12.5, translate="sven_warcry", translate2="sven_shield"})
 	caster:PerformAttack(enemy_target, true, true, true, false, true, false, false)
-	print("extra attack sent!")
+
 	attacks_sent = attacks_sent + 1	
 		return .3
 	end
