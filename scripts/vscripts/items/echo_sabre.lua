027108bbbdbe796ac4559079df73fe80d62e4c77
Unknown <superdotaplaya@gmail.com>
2020-01-07 13:30:21 -0500

    Added tooltips to auto mek, titan slain buff

--- scripts/vscripts/items/echo_sabre.lua
+++ scripts/vscripts/items/echo_sabre.lua
@@ -8,13 +8,13 @@ local enemy_target = keys.target
 local abil_cooldown = ability:GetSpecialValueFor("AbilityCooldown")
 
 if ability:GetCooldownTimeRemaining() == 0 then
-	ability:ApplyDataDrivenModifier(caster, enemy_target, "modifier_echo_sabre_2_debuff", {duration = .8})
 	ability:StartCooldown(5)
 	local attacks_sent = 0
 
 
 	Timers:CreateTimer(.3, function()
 	if attacks_sent <= 1 then
+		ability:ApplyDataDrivenModifier(caster, enemy_target, "modifier_echo_sabre_2_debuff", {duration = .8})
 		 StartAnimation(caster, {duration=1.5, activity=ACT_DOTA_ATTACK, rate=12.5, translate="sven_warcry", translate2="sven_shield"})
 	caster:PerformAttack(enemy_target, true, true, true, false, true, false, false)
 
