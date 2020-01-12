1fb92c8bc7f0eefdd8df7954bc6ffb0f19d753a4
Unknown <alex.x.eye@gmail.com>
2020-01-11 21:30:29 -0500

    midas 2 buff

--- scripts/vscripts/items/midas2.lua
+++ scripts/vscripts/items/midas2.lua
@@ -1,6 +1,6 @@
 function gpmBoost(keys)
 local caster = keys.caster
-PlayerResource:ModifyGold(caster:GetPlayerID(), 3, true, 10)
+PlayerResource:ModifyGold(caster:GetPlayerID(), 5, true, 10)
 if caster:HasItemInInventory("item_midas_2") and not caster:HasModifier("modifier_damage_pct") then
 	keys.ability:ApplyDataDrivenModifier(caster, caster, "modifier_damage_pct", {duration=1.1})
 end
