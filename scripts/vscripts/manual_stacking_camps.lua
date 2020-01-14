876b362fe065548b51d84855b9ecde77f9f4acd4
Unknown <superdotaplaya@gmail.com>
2020-01-14 18:18:03 -0500

    manual stacking camps now work as intended and level up each camp indivudally

--- scripts/vscripts/manual_stacking_camps.lua
+++ scripts/vscripts/manual_stacking_camps.lua
@@ -28,6 +28,35 @@ _G.radiant2_stacking_can_spawn = true
 print("radiant 2 is empty")
 end
 
+function disable_spawning_radiant3(trigger)
+_G.radiant3_stacking_can_spawn = false
+print("radiant 3 has creeps in it")
+end
+
+function enable_spawning_radiant3(trigger)
+_G.radiant3_stacking_can_spawn = true
+print("radiant 3 is empty")
+end
+
+function disable_spawning_radiant4(trigger)
+_G.radiant4_stacking_can_spawn = false
+print("radiant 4 has creeps in it")
+end
+
+function enable_spawning_radiant4(trigger)
+_G.radiant4_stacking_can_spawn = true
+print("radiant 4 is empty")
+end
+
+function disable_spawning_radiant5(trigger)
+_G.radiant5_stacking_can_spawn = false
+print("radiant 5 has creeps in it")
+end
+
+function enable_spawning_radiant5(trigger)
+_G.radiant5_stacking_can_spawn = true
+print("radiant 5 is empty")
+end
 function enable_spawning_dire2(trigger)
 _G.dire1_stacking_can_spawn = true
 print("dire 2 is empty")
@@ -36,4 +65,34 @@ end
 function disable_spawning_dire2(trigger)
 _G.dire2_stacking_can_spawn = false
 print("dire 2 has creeps in it")
+end
+
+function enable_spawning_dire3(trigger)
+_G.dire3_stacking_can_spawn = true
+print("dire 3 is empty")
+end
+
+function disable_spawning_dire3(trigger)
+_G.dire3_stacking_can_spawn = false
+print("dire 3 has creeps in it")
+end
+
+function enable_spawning_dire4(trigger)
+_G.dire4_stacking_can_spawn = true
+print("dire 4 is empty")
+end
+
+function disable_spawning_dire4(trigger)
+_G.dire4_stacking_can_spawn = false
+print("dire 4 has creeps in it")
+end
+
+function enable_spawning_dire5(trigger)
+_G.dire5_stacking_can_spawn = true
+print("dire 5 is empty")
+end
+
+function disable_spawning_dire5(trigger)
+_G.dire5_stacking_can_spawn = false
+print("dire 5 has creeps in it")
 end
