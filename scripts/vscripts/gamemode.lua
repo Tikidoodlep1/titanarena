62b5fbe8505c4c990332f5d9caaef5d192bb34bb
Unknown <superdotaplaya@gmail.com>
2020-01-14 20:39:59 -0500

    added a step to check what map is being played to avoid errors with the old map

--- scripts/vscripts/gamemode.lua
+++ scripts/vscripts/gamemode.lua
@@ -284,6 +284,35 @@ if _G.radiant2_stacking_can_spawn == true then
 				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
 		unit:CreatureLevelUp(_G.radiant2_stacks_cleared)
 end
+
+if _G.dire1_stacking_can_spawn == true then
+			_G.dire1_stacks_cleared = _G.dire1_stacks_cleared + 1
+	local point = Entities:FindByName(nil,"dire_stacking_spawn1"):GetAbsOrigin()
+
+		local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
+		unit:CreatureLevelUp(_G.dire1_stacks_cleared)
+				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
+		unit:CreatureLevelUp(_G.dire1_stacks_cleared)
+				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
+		unit:CreatureLevelUp(_G.dire1_stacks_cleared)
+
+
+end
+
+if _G.dire2_stacking_can_spawn == true then
+			_G.dire2_stacks_cleared = _G.dire2_stacks_cleared + 1
+	local point = Entities:FindByName(nil,"dire_stacking_spawn2"):GetAbsOrigin()
+
+		local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
+		unit:CreatureLevelUp(_G.dire2_stacks_cleared)
+				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
+		unit:CreatureLevelUp(_G.dire2_stacks_cleared)
+				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
+		unit:CreatureLevelUp(_G.dire2_stacks_cleared)
+
+end
+
+if GetMapName() == "5v5_h3rsh3y" then
 if _G.radiant3_stacking_can_spawn == true then
 	_G.radiant3_stacks_cleared = _G.radiant3_stacks_cleared + 1
 	local point = Entities:FindByName(nil,"radiant_stacking_spawn3"):GetAbsOrigin()
@@ -320,32 +349,7 @@ if _G.radiant5_stacking_can_spawn == true then
 		
 end
 
-if _G.dire1_stacking_can_spawn == true then
-			_G.dire1_stacks_cleared = _G.dire1_stacks_cleared + 1
-	local point = Entities:FindByName(nil,"dire_stacking_spawn1"):GetAbsOrigin()
 
-		local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
-		unit:CreatureLevelUp(_G.dire1_stacks_cleared)
-				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
-		unit:CreatureLevelUp(_G.dire1_stacks_cleared)
-				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
-		unit:CreatureLevelUp(_G.dire1_stacks_cleared)
-
-
-end
-
-if _G.dire2_stacking_can_spawn == true then
-			_G.dire2_stacks_cleared = _G.dire2_stacks_cleared + 1
-	local point = Entities:FindByName(nil,"dire_stacking_spawn2"):GetAbsOrigin()
-
-		local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
-		unit:CreatureLevelUp(_G.dire2_stacks_cleared)
-				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
-		unit:CreatureLevelUp(_G.dire2_stacks_cleared)
-				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
-		unit:CreatureLevelUp(_G.dire2_stacks_cleared)
-
-end
 if _G.dire3_stacking_can_spawn == true then
 			_G.dire3_stacks_cleared = _G.dire3_stacks_cleared + 1
 	local point = Entities:FindByName(nil,"dire_stacking_spawn3"):GetAbsOrigin()
@@ -378,6 +382,7 @@ if _G.dire5_stacking_can_spawn == true then
 		unit:CreatureLevelUp(_G.dire5_stacks_cleared)
 				local unit = CreateUnitByName("npc_manual_camp_creep", point, true, nil, nil, 4)
 		unit:CreatureLevelUp(_G.dire5_stacks_cleared)
+end
 end
 end
 
