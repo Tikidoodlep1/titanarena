c36a669f2416954aac598d5dd8f93e2ac959d821
Unknown <superdotaplaya@gmail.com>
2020-01-21 01:43:07 -0500

    Added siphon mail, needs icons and a bit more testing (only returns right click damage)

---
+++ scripts/vscripts/items/siphon_mail.lua
@@ -0,0 +1,38 @@
+require('libraries/playertables')
+function apply_siphon_damage_modifier( keys )
+local caster = keys.caster
+local caster_team = caster:GetTeamNumber()
+local ability = keys.ability
+local loc = caster:GetAbsOrigin()
+local distance = ability:GetSpecialValueFor("range")
+local abil_duration = ability:GetSpecialValueFor("duration")
+local ally_units = FindUnitsInRadius(caster_team, loc, nil, distance,DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+for num,unit in pairs(ally_units) do
+ability:ApplyDataDrivenModifier(caster, unit, "damage_soak", {duration = abil_duration})
+if unit == caster then
+ability:ApplyDataDrivenModifier(caster, caster, "damage_receiver", {duration = abil_duration})
+	end
+end
+
+end
+
+function send_damage(keys)
+local damage_taker = keys.caster
+local damage_dealer = keys.attacker
+local heroes = HeroList:GetAllHeroes()
+PrintTable(keys)
+for num, hero in pairs(heroes) do
+if hero:HasModifier("damage_receiver") and hero:GetTeamNumber() == damage_taker:GetTeamNumber() then
+	hero:RemoveModifierByName("damage_soak")
+local damageTable = {
+	victim = hero,
+	attacker = damage_dealer,
+	damage = (damage_dealer:GetAverageTrueAttackDamage(damage_taker) * .25),
+	damage_type = DAMAGE_TYPE_PURE,
+}
+
+ApplyDamage(damageTable)
+end
+end
+
+end
