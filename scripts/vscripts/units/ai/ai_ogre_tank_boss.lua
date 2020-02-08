dd01f6155d5c491f69889f0e7ded1042243ce658
Unknown <h3rsh3yb3ar@gmail.com>
2020-02-08 16:29:29 -0500

    ogre boss AI fix

--- scripts/vscripts/units/ai/ai_ogre_tank_boss.lua
+++ scripts/vscripts/units/ai/ai_ogre_tank_boss.lua
@@ -1,4 +1,5 @@
-
+LinkLuaModifier("ogre_tank_boss_jump_smash", "abilities/siltbreaker/npc_dota_creature_ogre_tank_boss/ogre_tank_boss_jump_smash.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("ogre_tank_boss_melee_smash", "abilities/siltbreaker/npc_dota_creature_ogre_tank_boss/ogre_tank_boss_melee_smash.lua", LUA_MODIFIER_MOTION_NONE)
 function Spawn( entityKeyValues )
 	if not IsServer() then
 		return
@@ -8,12 +9,11 @@ function Spawn( entityKeyValues )
 		return
 	end
 
-	--thisEntity.vInitialSpawnPos = thisEntity:GetOrigin()
-
-	thisEntity:AddNewModifier( nil, nil, "modifier_invulnerable", { duration = -1 } )
-
 	SmashAbility = thisEntity:FindAbilityByName( "ogre_tank_boss_melee_smash" )
 	JumpAbility = thisEntity:FindAbilityByName( "ogre_tank_boss_jump_smash" )
+
+	thisEntity.hasteFactor = 1
+	thisEntity.jumpChance = 80
 
 	thisEntity:SetContextThink( "OgreTankBossThink", OgreTankBossThink, 1 )
 end
@@ -27,50 +27,42 @@ function OgreTankBossThink()
 		return 1
 	end
 
-	if not thisEntity.bInitialized then
-		thisEntity.vInitialSpawnPos = thisEntity:GetOrigin()
-		thisEntity.bInitialized = true
-	end
-
-	if thisEntity.bStarted == false then
-		return 0.1
-	else
-		thisEntity:RemoveModifierByName( "modifier_invulnerable" )
-		--print( "removed invuln modifier from ogre boss" )
-	end
-
-	-- Are we too far from our initial spawn position?
-	local fDist = ( thisEntity:GetOrigin() - thisEntity.vInitialSpawnPos ):Length2D()
-	if fDist > 2500 then
-		RetreatHome()
-		return 2.0
-	end
-
-	local nEnemiesRemoved = 0
-	local enemies = FindUnitsInRadius( 3, thisEntity:GetOrigin(), nil, 1200, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE, FIND_CLOSEST, false )
+	local closeEnemy = false
+	local enemies = FindUnitsInRadius(thisEntity:GetTeamNumber(),
+									  thisEntity:GetOrigin(),
+									   nil,
+									   5000,
+									   DOTA_UNIT_TARGET_TEAM_ENEMY,
+									   DOTA_UNIT_TARGET_HERO,
+									   DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+									   FIND_CLOSEST,
+									   false)
 	for i = 1, #enemies do
 		local enemy = enemies[i]
 		if enemy ~= nil then
 			local flDist = ( enemy:GetOrigin() - thisEntity:GetOrigin() ):Length2D()
 			if flDist < 300 then
-				nEnemiesRemoved = nEnemiesRemoved + 1
-				table.remove( enemies, i )
+				closeEnemy = true
 			end
 		end
 	end
 
-	if JumpAbility ~= nil and JumpAbility:IsFullyCastable() and nEnemiesRemoved > 0 then
-		return Jump()
+	if JumpAbility ~= nil and JumpAbility:IsFullyCastable() and closeEnemy then
+		if thisEntity.jumpChance > RandomInt(1,100) then
+			return Jump()
+		end
 	end
 
 	if #enemies == 0 then
-		-- @todo: Could check whether there are ogre magi nearby that I should be positioning myself next to.  Either that or have the magi come to me.
+		print("No nearby enemies")
 		return 1
 	end
 
 	if SmashAbility ~= nil and SmashAbility:IsFullyCastable() then
 		return Smash( enemies[ 1 ] )
 	end
+
+	print("Nothing")
 	
 	return 0.5
 end
@@ -84,7 +76,7 @@ function Jump()
 		Queue = false,
 	})
 	
-	return 2.5
+	return 3.0
 end
 
 
@@ -98,24 +90,16 @@ function Smash( enemy )
 		thisEntity:AddNewModifier( thisEntity, nil, "modifier_provide_vision", { duration = 1.5 } )
 	end
 
+	-- local direction = enemy:GetForwardVector():Normalized()
+ --    local smashPrediction = enemy:GetAbsOrigin() + direction * RandomInt(0,300)
+
 	ExecuteOrderFromTable({
 		UnitIndex = thisEntity:entindex(),
 		OrderType = DOTA_UNIT_ORDER_CAST_POSITION,
 		AbilityIndex = SmashAbility:entindex(),
-		Position = enemy:GetOrigin(),
+		Position = enemy:GetAbsOrigin(),
 		Queue = false,
 	})
 
-	return 3 / thisEntity:GetHasteFactor()
+	return 3.0 / thisEntity.hasteFactor
 end
-
-function RetreatHome()
-	--print( "OgreTankBoss - RetreatHome()" )
-
-	ExecuteOrderFromTable({
-		UnitIndex = thisEntity:entindex(),
-		OrderType = DOTA_UNIT_ORDER_MOVE_TO_POSITION,
-		Position = thisEntity.vInitialSpawnPos
-	})
-end
-
