cb3471991248f4cd17e68edf739a228169963aa0
Unknown <superdotaplaya@gmail.com>
2020-02-08 01:00:10 -0500

    Venomancers level 25 talent is now fixed

--- scripts/vscripts/plague_wards.lua
+++ scripts/vscripts/plague_wards.lua
@@ -4,16 +4,21 @@ local ability = keys.ability
 local abil_level = keys.ability:GetLevel()
 local ward_hp = ability:GetSpecialValueFor("ward_hp_tooltip")
 local ward_damage = ability:GetSpecialValueFor("ward_damage_tooltip")
+local talent = caster:FindAbilityByName("special_bonus_unique_venomancer")
+local talentlvl = talent:GetLevel()
+local bonus = 1
 local point = caster:GetCursorPosition()
 local unit = CreateUnitByName("npc_dota_venomancer_plague_ward_1", point, true, caster, caster, caster:GetTeamNumber())
 EmitSoundOnLocationWithCaster(point, "Hero_Venomancer.Plague_Ward", caster)
-
+if talentlvl == 1 then
+bonus = 3
+end
 unit:SetControllableByPlayer(caster:GetPlayerID(), false)
 
-unit:SetMaxHealth(ward_hp)
+unit:SetMaxHealth(ward_hp*bonus)
 unit:SetHealth(unit:GetMaxHealth())
-unit:SetBaseDamageMax(ward_damage)
-unit:SetBaseDamageMin(ward_damage - 5)
+unit:SetBaseDamageMax(ward_damage*bonus)
+unit:SetBaseDamageMin(ward_damage*bonus)
 Timers:CreateTimer(40, function()
 
 unit:ForceKill(false)
