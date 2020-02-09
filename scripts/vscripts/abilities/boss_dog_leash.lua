be31e7308a48a53bffca23c37eacf99ffaeb75b8
Unknown <alex.x.eye@gmail.com>
2020-02-09 16:44:08 -0600

    dog leash progress

--- scripts/vscripts/abilities/boss_dog_leash.lua
+++ scripts/vscripts/abilities/boss_dog_leash.lua
@@ -16,25 +16,27 @@ end
 
 function dog_leash:OnSpellStart()
 	EmitSoundOn("Hero_Magnataur.Skewer.Cast", self:GetCaster())
+	local dog = self:GetCaster()
 	local dist = (thisEntity:GetOrigin() - self.vInitialSpawnPos):Length2D
-	self:SetBaseMoveSpeed(400)
-	local heroes = FindUnitsInRadius(4, self:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
-	self:MoveToPosition(heroes[0]:GetAbsOrigin())
-	self:AddNewModifier(self, "boss_dog_leash", "modifier_phased", {duration=1})
+	dog:SetBaseMoveSpeed(400)
+	local heroes = FindUnitsInRadius(4, dog:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+	dog:MoveToPosition(heroes[0]:GetAbsOrigin())
+	TouchingHero()
+	dog:AddNewModifier(dog, "boss_dog_leash", "modifier_phased", {duration=1})
 	Timers:CreateTimer(0, function()
 		if dist >= 600 then
-			self:AddNewModifier(self, "boss_dog_leash", "modifier_stunned", {duration=4})
-			self:SetBaseMoveSpeed(320)
+			dog:AddNewModifier(dog, "boss_dog_leash", "modifier_stunned", {duration=4})
+			dog:SetBaseMoveSpeed(320)
 			Timers:CreateTimer(4, function()
-				self:MoveToPosition(self.vInitialSpawnPos)
+				dog:MoveToPosition(self.vInitialSpawnPos)
 			end)
 			Timers.removeSelf = true
 		elseif thisEntity:GetAbsOrigin() - heroes[0]:GetAbsOrigin() == thisEntity:GetAbsOrigin() then
-			local damageTable = {victim = heroes[0], attacker = self, damage = 300, damage_type = DAMAGE_TYPE_PURE, damage_flags = DOTA_DAMAGE_FLAG_NONE, ability = self:FindAbilityByName("boss_dog_leash")}
+			local damageTable = {victim = heroes[0], attacker = dog, damage = 300, damage_type = DAMAGE_TYPE_PURE, damage_flags = DOTA_DAMAGE_FLAG_NONE, ability = dog:FindAbilityByName("boss_dog_leash")}
 			ApplyDamage(damageTable)
-			self:SetBaseMoveSpeed(320)
-			self:MoveToPositionAggressive(heroes[0]:GetAbsOrigin)
-			heroes[0]:AddNewModifier(self, "boss_dog_leash", "modifier_stunned", {duration=2})
+			dog:SetBaseMoveSpeed(320)
+			dog:MoveToPositionAggressive(heroes[0]:GetAbsOrigin)
+			heroes[0]:AddNewModifier(dog, "boss_dog_leash", "modifier_stunned", {duration=2})
 			Timers.removeSelf = true
 		end
 		return 0.3
@@ -43,14 +45,23 @@ end
 
 -----------------------------------------------------------------------------
 
-function TouchingHero(caster)
-	Timers:CreateTimer(0, function()
-		touched = FindUnitsInRadius(4, caster:GetAbsOrigin(), nil, 100, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
-		if touched then
-			for _, hero in pairs(touched) do
-				hero:AddNewModifier(self, "boss_dog_leash", "modifier_stunned", {duration=1.5})
+function TouchingHero()
+local dog = self:GetCaster()
+local endtouched = false
+	Timers:CreateTimer(1.5, function()
+	
+		Timers:CreateTimer(0, function()
+			touched = FindUnitsInRadius(4, dog:GetAbsOrigin(), nil, 100, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+			if touched then
+				for _, hero in pairs(touched) do
+					hero:AddNewModifier(dog, "boss_dog_leash", "modifier_stunned", {duration=1.5})
+				end
 			end
-		end
-		return 0.2
+			if endtouched == true then
+				Timers.removeSelf = true
+			end
+			return 0.2
+		end)
+		endtouched = true
 	end)
 end
